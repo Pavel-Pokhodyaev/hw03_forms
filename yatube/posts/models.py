438f96869a44ac4650987a582fafb07a660bff3c
Pavel-Pokhodyaev <pavka_91@mail.ru>
2022-07-13 16:57:31 +0300

    3

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -15,7 +15,7 @@ class Group(models.Model):
 
 
 class Post(models.Model):
-    text = models.TextField()
+    text = models.TextField('Текст поста', help_text='Введите текст поста')
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         User,
